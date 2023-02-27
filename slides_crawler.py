28bbee5c3adc6e75e622810d06ed8eaa04745566
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2023-02-26 17:01:26 -0800

    use argv for URL, ignore PDF

--- slides_crawler.py
+++ slides_crawler.py
@@ -3,13 +3,15 @@
 # crawl 572 slides to pdf
 # just put Prof saty's web slide url in url variable
 
+from sys import argv
+
 import img2pdf
 import requests
 from bs4 import BeautifulSoup
 
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
-url = ""
+url = argv[1]
 
 soup = BeautifulSoup(requests.get(url, headers=HEADER).text, "html.parser")
 
@@ -19,11 +21,7 @@ title = slides[0].table.tr.td.h1.text.strip()
 
 base_url = url[:url.rfind('/')+1]
 
-img_data = []
-for slide in slides[1:]:
-    img_url = slide.p.img['src']
-    abs_url = base_url + img_url
-    img_data.append(requests.get(abs_url).content)
+img_data = [requests.get(base_url + slide.p.img['src']).content for slide in slides[1:]]
 
 with open(title+".pdf", "wb") as f:
     f.write(img2pdf.convert(img_data))
